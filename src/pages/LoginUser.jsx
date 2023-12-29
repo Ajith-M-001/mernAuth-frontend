b3d6039cc7fd88e777cd5487be9290eb61bbad8d
Ajith-M-001 <ajithguru741@gmail.com>
2023-12-29 12:25:34 +0530

    add onchange and onsubmit functionality to register form and login form

--- src/pages/LoginUser.jsx
+++ src/pages/LoginUser.jsx
@@ -5,32 +5,47 @@ import { useState } from "react";
 
 const LoginUser = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleChange = () => {
+  const handleshowPassword = () => {
     setShowPassword(!showPassword);
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <div className="max-w-sm mx-auto mt-10 border shadow-sm  rounded-md p-5">
       <h1 className="text-2xl font-bold text-center my-3">welcome Back !!</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="relative">
           <TfiEmail className="absolute text-lg top-1/2 -translate-y-1/2 left-3" />
           <input
             className="w-full pl-10 my-1 px-4 py-2 rounded-md outline-none border focus-visible:border-gray-600"
             type="email"
             placeholder="Enter Your Email"
+            onChange={handleChange}
+            name="email"
           />
         </div>
         <div className="relative">
           {!showPassword ? (
             <FaRegEyeSlash
-              onClick={handleChange}
+              onClick={handleshowPassword}
               className="absolute cursor-pointer text-lg top-1/2 -translate-y-1/2 left-3"
             />
           ) : (
             <FaRegEye
-              onClick={handleChange}
+              onClick={handleshowPassword}
               className="absolute cursor-pointer text-lg top-1/2 -translate-y-1/2 left-3"
             />
           )}
@@ -39,6 +54,8 @@ const LoginUser = () => {
             className="w-full my-1 pl-10 px-4 py-2 rounded-md outline-none border focus-visible:border-gray-600"
             type={!showPassword ? "password" : "text"}
             placeholder="Enter Password"
+            onChange={handleChange}
+            name="password"
           />
         </div>
 
--- src/pages/RegisterUser.jsx
+++ src/pages/RegisterUser.jsx
@@ -6,26 +6,44 @@ import { useState } from "react";
 const RegisterUser = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  });
 
-  const handleChange = () => {
+  const handleshowPassword = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleConfirmChange = () => {
+  const handleshowConfirmPassword = () => {
     setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
   };
   return (
     <div className="max-w-sm mx-auto mt-10 border shadow-sm  rounded-md p-5">
       <h1 className="text-2xl font-bold text-center my-3">
         create new account
       </h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="relative">
           <FaRegUser className="absolute text-lg top-1/2 -translate-y-1/2 left-3" />
           <input
             className="w-full my-1 pl-10 px-4 py-2 rounded-md outline-none border focus-visible:border-gray-600"
             type="text"
             placeholder="Enter Your Name"
+            onChange={handleChange}
+            name="name"
           />
         </div>
         <div className="relative">
@@ -34,17 +52,19 @@ const RegisterUser = () => {
             className="w-full pl-10 my-1 px-4 py-2 rounded-md outline-none border focus-visible:border-gray-600"
             type="email"
             placeholder="Enter Your Email"
+            onChange={handleChange}
+            name="email"
           />
         </div>
         <div className="relative">
           {!showPassword ? (
             <FaRegEyeSlash
-              onClick={handleChange}
+              onClick={handleshowPassword}
               className="absolute cursor-pointer text-lg top-1/2 -translate-y-1/2 left-3"
             />
           ) : (
             <FaRegEye
-              onClick={handleChange}
+              onClick={handleshowPassword}
               className="absolute cursor-pointer text-lg top-1/2 -translate-y-1/2 left-3"
             />
           )}
@@ -53,17 +73,19 @@ const RegisterUser = () => {
             className="w-full my-1 pl-10 px-4 py-2 rounded-md outline-none border focus-visible:border-gray-600"
             type={!showPassword ? "password" : "text"}
             placeholder="Enter Password"
+            onChange={handleChange}
+            name="password"
           />
         </div>
         <div className="relative">
           {!showConfirmPassword ? (
             <FaRegEyeSlash
-              onClick={handleConfirmChange}
+              onClick={handleshowConfirmPassword}
               className="absolute cursor-pointer text-lg top-1/2 -translate-y-1/2 left-3"
             />
           ) : (
             <FaRegEye
-              onClick={handleConfirmChange}
+              onClick={handleshowConfirmPassword}
               className="absolute cursor-pointer text-lg top-1/2 -translate-y-1/2 left-3"
             />
           )}
@@ -71,6 +93,8 @@ const RegisterUser = () => {
             className="w-full pl-10 my-1 px-4 py-2 rounded-md outline-none border focus-visible:border-gray-600"
             type={!showConfirmPassword ? "password" : "text"}
             placeholder="Confirm Password"
+            onChange={handleChange}
+            name="confirmPassword"
           />
         </div>
         <button className="w-full px-4 py-2 rounded-md text-white hover:bg-gray-600 bg-gray-500 my-1">
