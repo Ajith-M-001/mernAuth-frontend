62b75a12bdf7fa298ad900b088161dc68a3eab80
Ajith-M-001 <ajithguru741@gmail.com>
2023-12-29 20:15:38 +0530

    added online api

--- src/app/slices/apiSlice.jsx
+++ src/app/slices/apiSlice.jsx
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://mernauth-backend-36mb.onrender.com",
+});
 
 export const apiSlice = createApi({
   baseQuery,
