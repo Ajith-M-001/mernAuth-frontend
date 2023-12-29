d9c6d3c8246c1d133e5012ca094f42aa679ece73
Ajith-M-001 <ajithguru741@gmail.com>
2023-12-29 13:27:26 +0530

    Creating API endpoints - login

--- src/app/slices/userApiSlice.jsx
+++ src/app/slices/userApiSlice.jsx
@@ -11,9 +11,16 @@ const userApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/login`,
+        method: "POST",
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useRegisterMutation } = userApiSlice;
+export const { useRegisterMutation, useLoginMutation } = userApiSlice;
 
 export default userApiSlice;
