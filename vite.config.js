245ed1783740f27727f289841a2637f852dde856
Ajith-M-001 <ajithguru741@gmail.com>
2023-12-29 20:19:24 +0530

    changed config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4000,
-    proxy: {
-      "/api": {
-        target: "https://mernauth-backend-36mb.onrender.com/",
-        changeOrigin: true,
-      },
-    },
-  },
 });
